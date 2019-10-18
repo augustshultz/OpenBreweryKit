73c2e88e58135e968f6141cebe2dc066ba92ad9c
augustshultz <augustshultz@gmail.com>
2019-10-17 20:19:35 -0500

    Initial linting

--- Tests/OpenBreweryKitTests/OpenBreweryKitTests.swift
+++ Tests/OpenBreweryKitTests/OpenBreweryKitTests.swift
@@ -10,6 +10,6 @@ final class OpenBreweryKitTests: XCTestCase {
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testExample", testExample)
     ]
 }
--- Tests/OpenBreweryKitTests/XCTestManifests.swift
+++ Tests/OpenBreweryKitTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(OpenBreweryKitTests.allTests),
+        testCase(OpenBreweryKitTests.allTests)
     ]
 }
 #endif

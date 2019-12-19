1d3de04c9f00b0e56da5fc9d538fd00b33e20e5a
August Shultz <augustshultz@gmail.com>
2019-12-18 19:54:49 -0600

    Make use of expectations instead of semaphores

--- Tests/OpenBreweryKitTests/BreweryNetworkControllerTests.swift
+++ Tests/OpenBreweryKitTests/BreweryNetworkControllerTests.swift
@@ -7,57 +7,50 @@ import XCTest
 
 class BreweryNetworkControllerTests: XCTestCase {
 
-  func testSearchForBrewery() throws {
-    let url = URL(string: "https://api.openbrewerydb.org/breweries/search?query=dog")!
-    let directory = URL(fileURLWithPath: #file).deletingLastPathComponent()
-    let file = directory.appendingPathComponent("sample_breweries.json")
-    let data = try Data(contentsOf: file)
-    URLProtocolMock.urls = [url: data]
-    let configuration = URLSessionConfiguration.ephemeral
-    configuration.protocolClasses = [URLProtocolMock.self]
-    let session = URLSession(configuration: configuration)
-    let networkController = BreweryNetworkController(session: session)
-    var returned = false
-    let semaphore = DispatchSemaphore(value: 0)
-    networkController.searchForBreweries(searchText: "dog" ) { (result) in
-      switch result {
-      case .success(let breweries):
-        XCTAssertTrue(breweries.count > 0)
-      case .failure(let error):
-        XCTFail(error.localizedDescription)
-      }
-      returned = true
-      semaphore.signal()
+    func testSearchForBrewery() throws {
+        let url = URL(string: "https://api.openbrewerydb.org/breweries/search?query=dog")!
+        let directory = URL(fileURLWithPath: #file).deletingLastPathComponent()
+        let file = directory.appendingPathComponent("sample_breweries.json")
+        let data = try Data(contentsOf: file)
+        URLProtocolMock.urls = [url: data]
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolMock.self]
+        let session = URLSession(configuration: configuration)
+        let networkController = BreweryNetworkController(session: session)
+        let expectation = XCTestExpectation()
+        networkController.searchForBreweries(searchText: "dog" ) { (result) in
+            switch result {
+            case .success(let breweries):
+                XCTAssertTrue(breweries.count > 0)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1.0)
+
     }
-    _ = semaphore.wait()
-    XCTAssertTrue(returned, "The completion was never called")
 
-  }
-
-  func testGetBreweries() throws {
-    let url = URL(string: "https://api.openbrewerydb.org/breweries")!
-    let directory = URL(fileURLWithPath: #file).deletingLastPathComponent()
-    let file = directory.appendingPathComponent("sample_breweries.json")
-    let data = try Data(contentsOf: file)
-    URLProtocolMock.urls = [url: data]
-    let configuration = URLSessionConfiguration.ephemeral
-    configuration.protocolClasses = [URLProtocolMock.self]
-    let session = URLSession(configuration: configuration)
-    let networkController = BreweryNetworkController(session: session)
-    var returned = false
-    let semaphore = DispatchSemaphore(value: 0)
-    networkController.getBreweries { (result) in
-      switch result {
-      case .success(let breweries):
-        XCTAssertTrue(breweries.count > 0)
-      case .failure(let error):
-        XCTFail(error.localizedDescription)
-      }
-      returned = true
-      semaphore.signal()
+    func testGetBreweries() throws {
+        let url = URL(string: "https://api.openbrewerydb.org/breweries")!
+        let directory = URL(fileURLWithPath: #file).deletingLastPathComponent()
+        let file = directory.appendingPathComponent("sample_breweries.json")
+        let data = try Data(contentsOf: file)
+        URLProtocolMock.urls = [url: data]
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolMock.self]
+        let session = URLSession(configuration: configuration)
+        let networkController = BreweryNetworkController(session: session)
+        let expectation = XCTestExpectation()
+        networkController.getBreweries { (result) in
+            switch result {
+            case .success(let breweries):
+                XCTAssertTrue(breweries.count > 0)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
     }
-    _ = semaphore.wait()
-    XCTAssertTrue(returned, "The completion was never called")
-  }
-
 }

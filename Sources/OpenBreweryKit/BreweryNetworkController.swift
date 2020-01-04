d9e77a4c5fbcce16b81754ac3b74dbc63b75c7c5
August Shultz <augustshultz@gmail.com>
2020-01-03 19:06:55 -0600

    Refactor network controller to reduce code duplication

--- Sources/OpenBreweryKit/BreweryNetworkController.swift
+++ Sources/OpenBreweryKit/BreweryNetworkController.swift
@@ -10,50 +10,38 @@ enum BreweryNetworkControllerError: Error {
 
 struct BreweryNetworkController {
   let session: URLSession
-
+  
   init(session: URLSession = URLSession.shared) {
     self.session = session
   }
-
+  
   func getBreweries(_ completion: @escaping ((Result<[Brewery], Error>) -> Void)) {
-
+    
     guard let url = URL(string: "https://api.openbrewerydb.org/breweries") else {
       completion(.failure(BreweryNetworkControllerError.invalidURL))
       return
     }
-    let task = session.dataTask(with: url) { (data, _, error) in
-      if let error = error {
-        completion(.failure(error))
-        return
-      }
-      guard let data = data else {
-        completion(.failure(BreweryNetworkControllerError.noDataReturned))
-        return
-      }
-      do {
-        let decoder = JSONDecoder()
-        completion(.success(try decoder.decode([Brewery].self, from: data)))
-      } catch _ {
-        completion(.failure(BreweryNetworkControllerError.dataParsingError))
-        return
-      }
-    }
-    task.resume()
+    fetchBreweries(fromUrl: url, completion)
   }
-
+  
   func searchForBreweries(searchText: String, _ completion: @escaping ((Result<[Brewery], Error>) -> Void)) {
-
+    
     guard let url = URL(string: "https://api.openbrewerydb.org/breweries/search"),
       var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
-      completion(.failure(BreweryNetworkControllerError.invalidURL))
-      return
+        completion(.failure(BreweryNetworkControllerError.invalidURL))
+        return
     }
     urlComponents.queryItems = [URLQueryItem(name: "query", value: searchText)]
     guard let searchUrl = urlComponents.url else {
       completion(.failure(BreweryNetworkControllerError.invalidURL))
       return
     }
-    let task = session.dataTask(with: searchUrl) { (data, _, error) in
+    fetchBreweries(fromUrl: searchUrl, completion)
+  }
+  
+  func fetchBreweries(fromUrl url: URL, _ completion: @escaping ((Result<[Brewery], Error>) -> Void)) {
+    
+    let task = session.dataTask(with: url) { (data, _, error) in
       if let error = error {
         completion(.failure(error))
         return
